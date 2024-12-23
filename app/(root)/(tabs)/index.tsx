6f7f1036b6dd07da42805397aa23a9040ce0dd95
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2024-12-23 16:11:22 +0530

    initial sign-up

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,9 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10 text-orange-400">Welcome to ReState</Text>
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to ReState</Text>
       
-      <Link href="/sign_in">Sign In</Link>
+      <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
       <Link href="/properties/1">Property</Link>
